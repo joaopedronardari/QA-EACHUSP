a800a13cd6a9c76ae047a11957651733c8ad8074
João Pedro Nardari dos Santos <joaonardari@gmail.com>
2016-03-20 13:21:09 -0300

    Caso de testes 24 ao 33

--- src/x/WSTTtest.java
+++ src/x/WSTTtest.java
@@ -57,205 +57,166 @@ public class WSTTtest extends TestCase {
 	}
 	
 	@Test
-	public void testCalculaComAnoInicialBissexto(){ // Caso 8
+	public void testAnoInicialBissexto(){ // Caso 8
 		Float tarifa = wstt.calculaTarifa(29,02,2016,15,0,0,29,02,2016,15,20,0);
 		assertEquals(8F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaComAnoFinalBissexto(){ // Caso 9
+	public void testAnoFinalBissexto(){ // Caso 9
 		Float tarifa = wstt.calculaTarifa(28,02,2016,23,50,0,29,02,2016,0,10,0);
 		assertEquals(4F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaComAnoInicialBissextoInvalido(){ // Caso 10
+	public void testAnoInicialBissextoInvalido(){ // Caso 10
 		Float tarifa = wstt.calculaTarifa(29,02,2015,15,0,0,29,02,2015,15,20,0);
 		assertEquals(-1F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaComAnoFinalBissextoInvalido(){ // Caso 11
+	public void testAnoFinalBissextoInvalido(){ // Caso 11
 		Float tarifa = wstt.calculaTarifa(28,02,2015,23,50,0,29,02,2015,0,10,0);
 		assertEquals(-1F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaDiaInicialZero(){ // Caso 12
+	public void testDiaInicialZero(){ // Caso 12
 		  Float tarifa = wstt.calculaTarifa(0,2,2015,0,0,0,1,2,2015,0,0,0);
 		  assertEquals(-1F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaMesInicialZero(){ // Caso 13
+	public void testMesInicialZero(){ // Caso 13
 		  Float tarifa = wstt.calculaTarifa(1,0,2015,0,0,0,1,2,2015,0,0,0);
 		  assertEquals(-1F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaAnoInicialZero(){ // Caso 14
+	public void testAnoInicialZero(){ // Caso 14
 		  Float tarifa = wstt.calculaTarifa(1,2,0,0,0,0,1,2,2015,0,0,0);
 		  assertEquals(-1F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaHoraInicialNegativa(){ // Caso 15
+	public void testHoraInicialNegativa(){ // Caso 15
 		  Float tarifa = wstt.calculaTarifa(1,2,2015,-1,0,0,1,2,2015,0,0,0);
 		  assertEquals(-1F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaMinutoInicialNegativa(){ // Caso 16
+	public void testMinutoInicialNegativo(){ // Caso 16
 		  Float tarifa = wstt.calculaTarifa(1,2,2015,0,-1,0,1,2,2015,0,0,0);
 		  assertEquals(-1F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaSegundoInicialNegativa(){ // Caso 17
+	public void testSegundoInicialNegativo(){ // Caso 17
 		  Float tarifa = wstt.calculaTarifa(1,2,2015,0,0,-1,1,2,2015,0,0,0);
 		  assertEquals(-1F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaDiaFinalZero(){ // Caso 18
+	public void testDiaFinalZero(){ // Caso 18
 		  Float tarifa = wstt.calculaTarifa(1,2,2015,0,0,0,0,2,2015,0,0,0);
 		  assertEquals(-1F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaMesFinalZero(){ // Caso 19
+	public void testMesFinalZero(){ // Caso 19
 		  Float tarifa = wstt.calculaTarifa(1,2,2015,0,0,0,1,0,2015,0,0,0);
 		  assertEquals(-1F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaAnoFinalZero(){ // Caso 20
+	public void testAnoFinalZero(){ // Caso 20
 		  Float tarifa = wstt.calculaTarifa(1,2,2015,0,0,0,1,2,0,0,0,0);
 		  assertEquals(-1F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaHoraFinalNegativa(){ // Caso 21
+	public void testHoraFinalNegativa(){ // Caso 21
 		  Float tarifa = wstt.calculaTarifa(1,2,2015,0,0,0,1,2,2015,-1,0,0);
 		  assertEquals(-1F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaMinutoFinalNegativa(){ // Caso 22
+	public void testMinutoFinalNegativa(){ // Caso 22
 		  Float tarifa = wstt.calculaTarifa(1,2,2015,0,0,0,1,2,2015,0,-1,0);
 		  assertEquals(-1F, tarifa);
 	}
 	
 	@Test
-	public void testCalculaSegundoFinalNegativa(){ // Caso 23
+	public void testSegundoFinalNegativa(){ // Caso 23
 		  Float tarifa = wstt.calculaTarifa(1,2,2015,0,0,0,1,2,2015,0,0,-1);
 		  assertEquals(-1F, tarifa);
 	}
 	
+	@Test
+	public void testDiaInicialMaiorLimite() { // Caso 24
+		Float tarifa = wstt.calculaTarifa(32,1,2015,0,0,0,1,1,2015,0,0,0);
+		assertEquals(-1F, tarifa);
+	}
 	
+	@Test
+	public void testMesInicialMaiorLimite() { // Caso 25
+		Float tarifa = wstt.calculaTarifa(1,13,2015,0,0,0,1,2,2015,0,0,0);
+		assertEquals(-1F, tarifa);
+	}
 	
+	@Test
+	public void testHoraInicialMaiorLimite() { // Caso 26
+		Float tarifa = wstt.calculaTarifa(1,2,2015,24,0,0,1,2,2015,0,0,0);
+		assertEquals(-1F, tarifa);
+	}
 	
+	@Test
+	public void testMinutoInicialMaiorLimite() { // Caso 27
+		Float tarifa = wstt.calculaTarifa(1,2,2015,0,60,0,1,2,2015,0,0,0);
+		assertEquals(-1F, tarifa);
+	}
 	
-	// VITOR TESTES
+	@Test
+	public void testSegundoInicialMaiorLimite() { // Caso 28
+		Float tarifa = wstt.calculaTarifa(1,2,2015,0,0,60,1,2,2015,0,0,0);
+		assertEquals(-1F, tarifa);
+	}
+	
+	@Test
+	public void testDiaFinalMaiorLimite() { // Caso 29
+		Float tarifa = wstt.calculaTarifa(1,1,2015,0,0,0,32,1,2015,0,0,0);
+		assertEquals(-1F, tarifa);
+	}
+	
+	@Test
+	public void testMesFinalMaiorLimite() { // Caso 30
+		Float tarifa = wstt.calculaTarifa(1,2,2015,0,0,0,1,13,2015,0,0,0);
+		assertEquals(-1F, tarifa);
+	}
+	
+	@Test
+	public void testHoraFinalMaiorLimite() { // Caso 31
+		Float tarifa = wstt.calculaTarifa(1,2,2015,0,0,0,1,2,2015,24,0,0);
+		assertEquals(-1F, tarifa);
+	}
+	
+	@Test
+	public void testMinutoFinalMaiorLimite() { // Caso 32
+		Float tarifa = wstt.calculaTarifa(1,2,2015,0,0,0,1,2,2015,0,60,0);
+		assertEquals(-1F, tarifa);
+	}
+	
+	@Test
+	public void testSegundoFinalMaiorLimite() { // Caso 33
+		Float tarifa = wstt.calculaTarifa(1,2,2015,0,0,0,1,2,2015,0,0,60);
+		assertEquals(-1F, tarifa);
+	}
+	
 	@Test
 	public void testCalculaDuracaoLigacao(){
 		Float duracaoLigacao = wstt.calculaDuracaoLigacao(10, 1, 2016, 10, 0, 0, 
 				10, 1, 2016, 12, 30, 0);
 		assertEquals(150F, duracaoLigacao);
-	}
-	
-	
-	
-	@Test
-	public void testCalculaDataMaiorQue31(){
-		Float duracaoLigacao = wstt.calculaDuracaoLigacao(32, 1, 2016, 17, 30, 0, 
-				32, 1, 2016, 19, 30, 0);
-		assertEquals(-1F, duracaoLigacao);
-	}
-	
-	@Test
-	public void testCalculaDataNegativa(){
-		Float duracaoLigacao = wstt.calculaDuracaoLigacao(-1, 1, 2016, 17, 30, 0, 
-				-1, 1, 2016, 19, 30, 0);
-		assertEquals(-1F, duracaoLigacao);
-	}
-	
-	@Test
-	public void testCalculaMesZero(){
-		  Float duracaoLigacao = wstt.calculaDuracaoLigacao(10, 0, 2016, 10, 0, 0, 
-						10, 0, 2016, 12, 30, 0);
-
-		  assertEquals(-1F, duracaoLigacao);
-	}
-	
-	@Test
-	public void testCalculaMesMaiorQue12(){
-		  Float duracaoLigacao = wstt.calculaDuracaoLigacao(10, 20, 2016, 10, 0, 0, 
-					10, 10, 2016, 12, 30, 0);
-		assertEquals(-1F, duracaoLigacao);
-	}
-	
-	@Test
-	public void testCalculaMesNegativo(){
-		  Float duracaoLigacao = wstt.calculaDuracaoLigacao(10, -3, 2016, 10, 0, 0, 
-					10, -1, 2016, 12, 30, 0);
-		assertEquals(-1F, duracaoLigacao);
-	}
-	
-	@Test
-	public void testCalculaAnosNegativo(){
-		  Float duracaoLigacao = wstt.calculaDuracaoLigacao(10, 3, -2016, 10, 0, 0, 
-					10, 1, 2016, 12, 30, 0);
-		assertEquals(-1F, duracaoLigacao);
-	}
-	
-	@Test
-	public void testCalculaAnosZero(){
-		  Float duracaoLigacao = wstt.calculaDuracaoLigacao(10, 3, 0, 10, 0, 0, 
-					10, 2, 2016, 12, 30, 0);
-		assertEquals(-1F, duracaoLigacao);
-	}
-	
-	@Test
-	public void testCalculaHoraNegativo(){
-		  Float duracaoLigacao = wstt.calculaDuracaoLigacao(10, 3, 2016, -10, 0, 0, 
-					10, 1, 2016, 12, 30, 0);
-		assertEquals(-1F, duracaoLigacao);
-	}
-	
-	@Test
-	public void testCalculaHoraAcimade24(){
-		  Float duracaoLigacao = wstt.calculaDuracaoLigacao(10, 3, 0, 26, 0, 0, 
-					17, 2, 2016, 12, 30, 0);
-		assertEquals(-1F, duracaoLigacao);
-	}
-	
-	@Test
-	public void testCalculaMinutoNegativo(){
-		  Float duracaoLigacao = wstt.calculaDuracaoLigacao(10, 3, 2016, 10, -3, 0, 
-					10, 1, 2016, 12, 30, 0);
-		assertEquals(-1F, duracaoLigacao);
-	}
-	
-	@Test
-	public void testCalculaMinutoAcimade59(){
-		  Float duracaoLigacao = wstt.calculaDuracaoLigacao(10, 3, 0, 10, 60, 0, 
-					10, 2, 2016, 12, 30, 0);
-		assertEquals(-1F, duracaoLigacao);
-	}
-	
-	@Test
-	public void testCalculaSegundoNegativo(){
-		  Float duracaoLigacao = wstt.calculaDuracaoLigacao(10, 3, 2016, 10, 0, -6, 
-					10, 1, 2016, 12, 30, 0);
-		assertEquals(-1F, duracaoLigacao);
-	}
-	
-	@Test
-	public void testCalculaSegundoAcimade59(){
-		  Float duracaoLigacao = wstt.calculaDuracaoLigacao(10, 3, 0, 10, 0, 0, 
-					10, 2, 2016, 12, 30, 80);
-		assertEquals(-1F, duracaoLigacao);
 	}
 
 }
