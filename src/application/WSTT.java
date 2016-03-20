a0fa3b623ec3d95039ca1653b3bb32b7c1c28dbc
João Pedro Nardari dos Santos <joaonardari@gmail.com>
2016-03-20 15:54:19 -0300

    Codigo Simples

--- src/application/WSTT.java
+++ src/application/WSTT.java
@@ -15,11 +15,14 @@ public class WSTT {
 		boolean datasValidas = verify(diai, mesi, anoi, horai, minutoi, segundoi, diaf, mesf, anof, horaf, minutof, segundof);
 		if (!datasValidas) { return -1; }
 		
+		// Converte datas
 		final Date dataInicial = converterParaData(diai, mesi, anoi, horai, minutoi, segundoi);
 		final Date dataFinal = converterParaData(diaf, mesf, anof, horaf, minutof, segundof);
 		if (dataInicial == null || dataFinal == null) { return -1; }
 		
+		// Calc duracao
 		long duracaoLigacao = calculaDuracaoLigacao(dataInicial, dataFinal);
+		// Preco ainda sem considerar duracao + de 60
 		float preco = calculaPreco(dataInicial, dataFinal);
 		float tarifaFinal = aplicaDescontoSuperior1Hora(preco, duracaoLigacao);
 		return tarifaFinal;
@@ -32,49 +35,21 @@ public class WSTT {
 		Calendar fim = Calendar.getInstance();
 		fim.setTime(dataFinal);
 
-		System.out.println(duracao);
-		
-		/*Date inicioPeriodoComDesconto = converterParaData(inicio.get(Calendar.DAY_OF_MONTH), inicio.get(Calendar.MONTH)+1, inicio.get(Calendar.YEAR), 18, 0, 0);
-		Date inicioPeriodoMeiaNoite = converterParaData(inicio.get(Calendar.DAY_OF_MONTH), inicio.get(Calendar.MONTH)+1, inicio.get(Calendar.YEAR), 23, 59, 59);;
-		Date fimPeriodoComDesconto = converterParaData(fim.get(Calendar.DAY_OF_MONTH)+1, fim.get(Calendar.MONTH)+1, fim.get(Calendar.YEAR), 7, 59, 59);;
-		 */
-		Calendar aux = inicio;
-		float valor = 0F;
-		
+		// Checa se esta na faixa de desconto ou nao
 		if (inicio.get(Calendar.HOUR_OF_DAY) >= 8 && inicio.get(Calendar.HOUR_OF_DAY) < 18 &&
 				fim.get(Calendar.HOUR_OF_DAY) >= 8 && fim.get(Calendar.HOUR_OF_DAY) < 18) {
 			return calculoValorBruto(duracao);
 		} else {
 			return calculoValorBruto(duracao)*0.5F;
 		}
-		
-		/*
-		while(duracao>=0) {
-			if(aux.get(Calendar.HOUR_OF_DAY) >= 8 && aux.get(Calendar.HOUR_OF_DAY) < 18) {
-				long minutos = calculaDuracaoLigacao(aux.getTime(), inicioPeriodoComDesconto);
-				duracao-=minutos;
-				valor += calculoValorBruto(minutos);
-				aux.setTime(inicioPeriodoComDesconto);		
-			} else if(aux.get(Calendar.HOUR_OF_DAY) >= 0 && aux.get(Calendar.HOUR_OF_DAY) < 8) {
-				long minutos = calculaDuracaoLigacao(aux.getTime(), fimPeriodoComDesconto);
-				duracao-=minutos;
-				valor += (calculoValorBruto(minutos)*0.5);
-				aux.setTime(fimPeriodoComDesconto);	
-			} else if(aux.get(Calendar.HOUR_OF_DAY) >= 18 && aux.get(Calendar.HOUR_OF_DAY) < 24) {
-				long minutos = calculaDuracaoLigacao(aux.getTime(), inicioPeriodoMeiaNoite);
-				duracao-=minutos;
-				valor += (calculoValorBruto(minutos)*0.5);
-				aux.setTime(inicioPeriodoMeiaNoite);
-			}
-		}*/
-		
-		//return valor;
 	}
 	
+	// Calcula valor bruto - sem desconto
 	private float calculoValorBruto(long minutos) {
 		return (float) (minutos * 0.4);
 	}
 	
+	// Pega variaveis e transforma em data
 	public Date converterParaData(int dia,int mes, int ano, int hora, int minuto, int segundo) {
 		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss"); 
 		String dataString = dia + "-" + mes + "-" + ano + " " + hora + ":" + minuto + ":" + segundo;
@@ -87,34 +62,25 @@ public class WSTT {
 		}
 	}
 	
+	// Da a duracao em minutos
 	public long calculaDuracaoLigacao(final Date dataInicial, final Date dataFinal) {
 		long milliseconds = Math.abs(dataFinal.getTime() - dataInicial.getTime());
         return milliseconds / MINUTE_IN_MILLISECONDS;
 	}
-	
-	public Calendar atualizaCalendar(int dia, int mes, int ano, int hora, int min, int seg){
-		
-        final Calendar c = Calendar.getInstance();
-        c.set(Calendar.DAY_OF_MONTH, dia);
-        c.set(Calendar.MONTH, mes);
-        c.set(Calendar.YEAR, ano);
-        c.set(Calendar.HOUR_OF_DAY, hora);
-        c.set(Calendar.MINUTE, min);
-        c.set(Calendar.SECOND, seg);
-        return c;
-	}
 
+	// aplica desconto de superior a uma hora
 	public float aplicaDescontoSuperior1Hora(float precoBruto, long duracaoLigacao) {
 		boolean maisQueUmaHora = duracaoLigacao >= 60 ? true:false;
 		return maisQueUmaHora ? precoBruto * (1 - 0.15F) : precoBruto;
 	}
 	
-	//retorna a diferença em horas
+	//retorna a diferenca em horas
     public static long hoursBetween(Date dateFrom, Date dateTo) {
         long milliseconds = Math.abs(dateTo.getTime() - dateFrom.getTime());
         return milliseconds / HOUR_IN_MILLISECONDS;
     }
     
+    // valida data
     public boolean verify(int diai, int mesi, int anoi, int horai, int minutoi, int segundoi, int diaf, int mesf, int anof, int horaf, int minutof, int segundof){
     	  	
     	if(diai >= 31 || diai <= 0 || diaf >=31 || diaf <=0){
@@ -159,6 +125,7 @@ public class WSTT {
     	return true;
     }
     
+    // checa se eh ano bissexto
     public boolean bissexto(int ano) {
     	String yearString = String.valueOf(ano);
     	if(ano%4==0) {
